65a7d3d1f26d19119c9b59c47231be23b207a2d4
Mustafa Mohsen <mustafamohsen@gmail.com>
2026-05-25 15:07:33 +0300

    feat: expose rtler c ffi

---
+++ include/rtler.h
@@ -0,0 +1,15 @@
+#ifndef RTLER_H
+#define RTLER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *rtler_transform_text(const char *input);
+void rtler_free_string(char *ptr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
